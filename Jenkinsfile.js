10e165bd201c5e28cd41b86584fbaafa29be9609
Дмитрий Зайков <d.a.zaykov@yandex.ru>
2019-08-30 13:59:29 +0500

    to js

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -8,6 +8,7 @@ node {
   stage('Results') {
     junit '**/target/surefire-reports/TEST-*.xml'
     archiveArtifacts 'target/*.jar'
+    emailext body: 'qqqqqqqqqqq', subject: '[Jenkins] Build', to: 'd.a.zaykov@yandex.ru'
   }
 }
 
